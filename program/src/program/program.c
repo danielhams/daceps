0df3b8775cde2d1b3d8626e3ee209ff179ee9720
Daniel Hams <daniel.hams@gmail.com>
2018-08-15 06:28:08 +0100

    And a first attempt at a program project too.

---
+++ program/src/program/program.c
@@ -0,0 +1,13 @@
+#include <libraryone/libraryoneinterface.h>
+#include <librarytwo/librarytwointerface.h>
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char**argv)
+{
+    printf("Program called.\n");
+
+    return 0;
+}
