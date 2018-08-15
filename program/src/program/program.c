3985e775d948892ac0972461a1d83c13ce435154
Daniel Hams <daniel.hams@gmail.com>
2018-08-15 06:33:03 +0100

    And wire some calls to the libraries in there.

--- program/src/program/program.c
+++ program/src/program/program.c
@@ -9,5 +9,14 @@ int main(int argc, char**argv)
 {
     printf("Program called.\n");
 
+    printf("Fetching a string from library one ->%s\n",
+           libraryone_getl1string());
+
+    printf("Fetching a string from library two ->%s\n",
+           librarytwo_getl2string());
+
+    printf("Fetching a string from library two that calls l1 as well ->%s\n",
+           librarytwo_getl2andl1string());
+
     return 0;
 }
